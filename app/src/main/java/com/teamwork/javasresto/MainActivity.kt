74e834c947c872823b0a5854d799af689fd991f7
ryandhikabaa <ryandhikabintang0@gmail.com>
2021-12-26 05:48:28 +0700

    [Initialize] Creat Project

---
+++ app/src/main/java/com/teamwork/javasresto/MainActivity.kt
@@ -0,0 +1,12 @@
+
+package com.teamwork.javasresto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
