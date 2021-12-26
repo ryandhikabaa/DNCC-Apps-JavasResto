b107dd36a2f28418ba6950d5548ab63659fb118d
ryandhikabaa <ryandhikabintang0@gmail.com>
2021-12-26 07:18:17 +0700

    [XML] Dashboard XML

---
+++ app/src/main/java/com/teamwork/javasresto/activity/DashboardActivity.kt
@@ -0,0 +1,11 @@
+package com.teamwork.javasresto.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
